5c88e316249a95da15690a0e55dcf30d34daa691
Alexandr <saniabublik5@gmail.com>
2026-02-09 11:31:14 +0300

    feat: добавить форму авторизации

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import Input from "@/parts/Input";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "@/style/css/page_css/login.css";
 import Form from "@/modules/Form";
 import sum from "@/style/images/sun.png";
@@ -7,8 +7,51 @@ import sum from "@/style/images/sun.png";
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import loginData from "@/types/loginData";
+import { SuccessResponse, ErrorResponse } from "@/types/response";
+import loginUser from "@/api/login";
 
 function Login() {
+    const [error, setError] = useState('');
+    const [form, setForm] = useState<loginData>(
+        {
+            username: '',
+            password: '',
+        }
+    );
+
+    const usernameRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+
+    const handleSubmit = async (e: React.MouseEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        try {
+            const response: SuccessResponse | ErrorResponse = await loginUser(form);
+
+            console.log('response: ', response);
+
+            if (response.status === true) {
+                console.log('Успешно ' + response);
+                setError('');
+                localStorage.setItem('user', JSON.stringify(response.data));
+                // window.location.href = '/';
+            }else{
+                console.log('Ошибка ' + response.message);
+                setError(response.message);
+            }
+        } catch (error) {
+            console.error('Полная ошибка:', error);
+            throw error;
+        }
+    };
 
     useEffect(() => {
             AOS.init({
@@ -30,7 +73,7 @@ function Login() {
                     <div className="form-content" data-aos="fade-up" data-aos-duration="800" data-aos-delay="200">
                         <p className="brand" data-aos="fade-up" data-aos-duration="800" data-aos-delay="100">Inseptum</p>
                         <h1>Вход</h1>
-                        <h2 className="error"></h2>
+                        {error && <h2 className="error">{error}</h2>}
                         
                         <Form>
                             <Input 
@@ -38,17 +81,22 @@ function Login() {
                                 title="text" 
                                 placeholder="Введите ваше имя" 
                                 name="username"
+                                ref={usernameRef}
+                                onChange={handleChange}
                             />
                             <Input 
                                 type="password" 
                                 title="password" 
                                 placeholder="Введите пароль" 
                                 name="password"
+                                ref={passwordRef}
+                                onChange={handleChange}
                             />
                             <Input 
                                 type="submit" 
                                 title="submit" 
                                 value="Войти"
+                                onClick={handleSubmit}
                             />
                         </Form>
                         
