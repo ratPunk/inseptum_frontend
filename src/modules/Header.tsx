c2def1785fa9ce50878fc8ddd7a5afac5ac089db
Alexandr <saniabublik5@gmail.com>
2026-02-09 12:23:23 +0300

    feat: добавить отображение того что пользователь вошёл

--- src/modules/Header.tsx
+++ src/modules/Header.tsx
@@ -2,11 +2,22 @@ import Link from "@/parts/link";
 import "@/style/css/modules_css/header.css";
 import logo from "../style/svg/logo.svg"
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import userDataRespose from "@/types/userDatarespose";
+import { IoExit } from "react-icons/io5";
 
 
 function Header() {
     const location = useLocation();
+    const [user, setUser] = useState<userDataRespose | null>(null);
 
+
+    useEffect(() => {
+        const storeduser = localStorage.getItem('user');
+        if(storeduser !== null){
+            setUser(JSON.parse(storeduser));
+        }
+    })
 
     return (
         <header id="Header" >
@@ -16,14 +27,25 @@ function Header() {
                     <h1 className="logo-text">Inseptum</h1>
                 </div>
                 <nav className="nav">
-                    <Link href="/home" text="Главная" className={`header-link ${location.pathname === "/home" ? "active" : ""}`}/>
-                    <Link href="/" text="Статьи" className="header-link"/>
-                    <Link href="/" text="Тесты" className="header-link"/>
-                    <Link href="/" text="Задачи" className="header-link"/>
+                    <Link href="/home" text="Главная" className={`header-link ${location.pathname === "/home" ? "active" : ""}`} />
+                    <Link href="/" text="Статьи" className="header-link" />
+                    <Link href="/" text="Тесты" className="header-link" />
+                    <Link href="/" text="Задачи" className="header-link" />
                 </nav>
                 <div className="user-actions">
-                    <Link href="/login" text="Войти" className="auth"/>                    
-                    <Link href="/register" text="Зарегестрироваться" className="register"/>
+                    {user ? (
+                        <>
+                        <Link href="/profile" text={user.username} className="user-name" />
+                        |
+                        <Link href="/profile" text="Выход" className="exit" icon={IoExit}/>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/login" text="Войти" className="auth" />
+                            <Link href="/register" text="Зарегестрироваться" className="register" />
+                        </>
+                    )}
+
                 </div>
             </div>
         </header>
